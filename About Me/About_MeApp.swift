8f55afbb648bf7a041cc3b14f8074594693d801d
Bandit Silachai <mbandit001@yahoo.co.th>
2022-02-17 10:48:36 +0700

    Initial Commit

---
+++ About Me/About_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_MeApp.swift
+//  About Me
+//
+//  Created by Bandit Silachai on 17/2/22.
+//
+
+import SwiftUI
+
+@main
+struct About_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
